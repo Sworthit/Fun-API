2663a585fcb65a06c0d2dc3c00d56b8831e5dc5e
Sworthit <Maciej.rawluk@gmail.com>
2021-07-21 14:39:53 +0200

    Statistics avg, shortest and longest name added

---
+++ FunApi/Controllers/StatisticController.cs
@@ -0,0 +1,75 @@
+﻿using FunApi.Context;
+using FunApi.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StatisticController : Controller
+    {
+        private readonly ApiDBContext _context;
+
+        public StatisticController(ApiDBContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet("avg")]
+        public async Task<ServiceResponse<double>> GetAvgNameLength()
+        {
+            ServiceResponse<double> serviceResponse = new ServiceResponse<double>();
+            var nameList = await _context.GeneratedNames.ToListAsync();
+
+            if (nameList == null)
+            {
+                serviceResponse.Success = false;
+                serviceResponse.Message = "Could not fetch data";
+                return serviceResponse;
+            }
+            serviceResponse.Message = "Avarage name length calculated, names found : " + nameList.Count.ToString();
+            serviceResponse.Data = Statistic.CalculateAvgNameLength(nameList);
+
+            return serviceResponse;
+        }
+
+        [HttpGet("shortest")]
+        public async Task<ServiceResponse<GeneratedName>> GetShortestName()
+        {
+            ServiceResponse<GeneratedName> serviceResponse = new ServiceResponse<GeneratedName>();
+            var nameList = await _context.GeneratedNames.ToListAsync();
+
+            if (nameList == null)
+            {
+                serviceResponse.Success = false;
+                serviceResponse.Message = "Could not fetch data";
+                return serviceResponse;
+            }
+            serviceResponse.Data = Statistic.GetShortestName(nameList);
+
+            return serviceResponse;
+        }
+
+        [HttpGet("longest")]
+        public async Task<ServiceResponse<GeneratedName>> GetLongestName()
+        {
+            ServiceResponse<GeneratedName> serviceResponse = new ServiceResponse<GeneratedName>();
+            var nameList = await _context.GeneratedNames.ToListAsync();
+
+            if (nameList == null)
+            {
+                serviceResponse.Success = false;
+                serviceResponse.Message = "Could not fetch data";
+                return serviceResponse;
+            }
+            serviceResponse.Data = Statistic.GetLongestName(nameList);
+
+            return serviceResponse;
+        }
+    }
+}
---
+++ FunApi/Model/Statistic.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunApi.Model
+{
+    public class Statistic
+    {
+        public static double? AvgNameLength { get; set; }
+        
+        public static GeneratedName ShortestName { get; set; }
+        
+        public static GeneratedName LongestName { get; set; }
+
+        public static double CalculateAvgNameLength(List<GeneratedName> nameList)
+        {
+            if (AvgNameLength == null)
+            {
+                int sum = 0;
+                foreach (var name in nameList)
+                {
+                    sum += name.Name.Length;
+                }
+                AvgNameLength = sum / nameList.Count;
+            }
+            return AvgNameLength.Value;
+        }
+
+        public static GeneratedName GetShortestName(List<GeneratedName> nameList)
+        {
+            if (ShortestName == null)
+            {
+                var minName = nameList[0];
+                foreach (var name in nameList)
+                {
+                    if (name.Name.Length < minName.Name.Length)
+                    {
+                        minName = name;
+                    }
+                }
+                ShortestName = minName;
+            }
+            return ShortestName;
+        }
+
+        public static GeneratedName GetLongestName(List<GeneratedName> nameList)
+        {
+            if (LongestName == null)
+            {
+                var longName = nameList[0];
+                foreach (var name in nameList)
+                {
+                    if (name.Name.Length > longName.Name.Length)
+                    {
+                        longName = name;
+                    }
+                }
+                LongestName = longName;
+            }
+            return LongestName;
+        }
+    }
+}
